f96b5cc977756c2a2c351dd3bb0c0a835b1c6953
lc87624 <lc87624@gmail.com>
2026-04-23 14:36:37 +0800

    exercises 3.2-3.8

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
+app.use(express.json())
+
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const persons = [
     { 
@@ -26,6 +32,51 @@ const persons = [
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+    `)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const personIndex = persons.findIndex(person => person.id === id)
+    if (personIndex !== -1) {
+        persons.splice(personIndex, 1)
+        response.status(204).end()
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.post('/api/persons', (request, response) => {
+    const { name, number } = request.body
+    
+    if (!name || !number) {
+        return response.status(400).json({ error: 'Name and number are required' })
+    }
+
+    if (persons.some(person => person.name === name)) {
+        return response.status(400).json({ error: 'Name must be unique' })
+    }
+
+    const id = Math.floor(Math.random() * 100000)
+    const newPerson = { id, name, number }
+    persons.push(newPerson)
+    response.status(201).json(newPerson)
 })
 
 const PORT = 3001
